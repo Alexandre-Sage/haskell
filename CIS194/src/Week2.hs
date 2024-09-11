393ea5c60b715a971c5e5d6c4adc61b9d6f50b0f
Alexandre Sage <you@example.com>
2024-09-11 09:00:26 +0200

    feat: week 2 ok

--- CIS194/src/Week2.hs
+++ CIS194/src/Week2.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE FlexibleInstances #-}
 
-module Week2 (LogMessage (..), MessageType (..), parseMessage, parse, MessageTree (..), insert, build) where
+module Week2 (LogMessage (..), MessageType (..), parseMessage, parse, MessageTree (..), insert, build, inorder, whatWentWrong) where
 
 import qualified Data.List as List
 import Helpers
@@ -47,11 +47,33 @@ getTimeStamp :: LogMessage -> TimeStamp
 getTimeStamp (Unknown _) = -1
 getTimeStamp (LogMessage _ x _) = x
 
+isUnknow :: LogMessage -> Bool
+isUnknow (LogMessage {}) = False
+isUnknow (Unknown _) = True
+
 insert :: LogMessage -> MessageTree -> MessageTree
 insert newLog Leaf = Node Leaf newLog Leaf
 insert newLog (Node left cur right)
+  | isUnknow newLog = Node left cur right
   | getTimeStamp newLog < getTimeStamp cur = Node (insert newLog left) cur right
   | otherwise = Node left cur $ insert newLog right
 
 build :: [LogMessage] -> MessageTree
 build = foldr insert Leaf
+
+inorder :: MessageTree -> [LogMessage]
+inorder Leaf = []
+inorder (Node left val right) = inorder left <> (val : inorder right)
+
+extractMessage :: LogMessage -> String
+extractMessage (Unknown msg) = msg
+extractMessage (LogMessage _ _ msg) = msg
+
+isOver50Error :: LogMessage -> Bool
+isOver50Error (LogMessage (Error x) _ _)
+  | x >= 50 = True
+  | otherwise = False
+isOver50Error _ = False
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong = List.map extractMessage . List.filter isOver50Error
--- CIS194/test/Week2Tests.hs
+++ CIS194/test/Week2Tests.hs
@@ -5,7 +5,7 @@ module Week2Tests (testsWeek2, testIoWeek2) where
 import qualified Data.List as List
 import Helpers (assertMessage)
 import Test.HUnit (Test (TestCase, TestLabel, TestList), Testable (test), assertEqual, failures)
-import Week2 (LogMessage (..), MessageTree (Leaf, Node), MessageType (Error, Info, Warning), build, parse, parseMessage)
+import Week2 (LogMessage (..), MessageTree (Leaf, Node), MessageType (Error, Info, Warning), build, inorder, parse, parseMessage, whatWentWrong)
 
 logs =
   [ ("I 6 Completed armadillo processing", LogMessage Info 6 "Completed armadillo processing"),
@@ -37,12 +37,32 @@ expectLogStrFormated =
     LogMessage (Error 99) 10 "Flange failed!"
   ]
 
+expectedSortedLog =
+  [ LogMessage Info 1 "Nothing to report",
+    LogMessage (Error 20) 2 "Too many pickles",
+    LogMessage (Error 70) 3 "Way too many pickles",
+    LogMessage Info 4 "Everything normal",
+    LogMessage Warning 5 "Flange is due for a check-up",
+    LogMessage Info 6 "Completed armadillo processing",
+    LogMessage Info 7 "Out for lunch, back in two time steps",
+    LogMessage (Error 65) 8 "Bad pickle-flange interaction detected",
+    LogMessage Info 9 "Back from lunch",
+    LogMessage (Error 99) 10 "Flange failed!",
+    LogMessage Info 11 "Initiating self-destruct sequence"
+  ]
+
 expectedTree =
   Node
     ( Node (Node (Node Leaf (LogMessage Info 1 "Nothing to report") Leaf) (LogMessage (Error 20) 2 "Too many pickles") (Node (Node (Node Leaf (LogMessage (Error 70) 3 "Way too many pickles") (Node Leaf (LogMessage Info 4 "Everything normal") Leaf)) (LogMessage Warning 5 "Flange is due for a check-up") (Node Leaf (LogMessage Info 6 "Completed armadillo processing") Leaf)) (LogMessage Info 7 "Out for lunch, back in two time steps") (Node Leaf (LogMessage (Error 65) 8 "Bad pickle-flange interaction detected") Leaf))) (LogMessage Info 9 "Back from lunch") Leaf
     )
     (LogMessage (Error 99) 10 "Flange failed!")
     (Node Leaf (LogMessage Info 11 "Initiating self-destruct sequence") Leaf)
+
+expectedFinalMessages =
+  [ "Way too many pickles",
+    "Bad pickle-flange interaction detected",
+    "Flange failed!"
+  ]
 
 parseMessageTest :: (String, LogMessage) -> Test
 parseMessageTest (testValue, expectedValue) =
@@ -62,6 +82,16 @@ buildTest file = do
   content <- readFile file
   return $ TestCase $ assertEqual "Should parse log content as binary tree" ((build . parse) content) expectedTree
 
+inOrderTest :: FilePath -> IO Test
+inOrderTest file = do
+  content <- readFile file
+  return $ TestCase $ assertEqual "Should sort" ((inorder . build . parse) content) expectedSortedLog
+
+whatWentWrongTest :: FilePath -> IO Test
+whatWentWrongTest file = do
+  content <- readFile file
+  return $ TestCase $ assertEqual "Should show what went wrong" ((whatWentWrong . inorder . build . parse) content) expectedFinalMessages
+
 testsWeek2 :: Test
 testsWeek2 = TestList [TestLabel "Parse message" $ parseMessageSuite logs]
 
@@ -70,4 +100,6 @@ testIoWeek2 =
   do
     test <- parseTest "test/input/log.txt"
     test2 <- buildTest "test/input/log.txt"
-    return $ TestList [TestLabel "parse" test, TestLabel "build" test2]
+    test3 <- inOrderTest "test/input/log.txt"
+    test4 <- whatWentWrongTest "test/input/log.txt"
+    return $ TestList [TestLabel "parse" test, TestLabel "build" test2, TestLabel "inorder" test3, TestLabel "what went wrong" test4]
